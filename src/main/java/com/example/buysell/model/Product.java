6d2d065239c4abb43adcca3ad994fdec12123619
ferty460 <rostovcevvladimir460@gmail.com>
2023-08-21 20:26:18 +0300

    бд, поиск товаров по названию

--- src/main/java/com/example/buysell/model/Product.java
+++ src/main/java/com/example/buysell/model/Product.java
@@ -2,15 +2,34 @@ package com.example.buysell.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description", columnDefinition = "text")
     private String description;
+
+    @Column(name = "price")
     private int price;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "author")
     private String author;
 }
--- src/main/java/com/example/buysell/service/ProductService.java
+++ src/main/java/com/example/buysell/service/ProductService.java
@@ -1,39 +1,35 @@
 package com.example.buysell.service;
 
 import com.example.buysell.model.Product;
+import com.example.buysell.repo.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
 
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID, "iphone 8", "descr", 18000, "Moscow", "kirill"));
-        products.add(new Product(++ID, "iphone 7", "descr", 16000, "Moscow", "olegg"));
-    }
-
-    public List<Product> listProducts() {
-        return products;
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
